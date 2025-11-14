6e608ddb19f443178d56d99bb05957ab283c718c
Yehor <ka.egor.ka@gmail.com>
2025-11-14 11:51:33 +0100

    neeko

---
+++ Neeko-main/lora.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+current_time=$(date +"%Y%m%d%H%M%S")
+
+python train.py \
+    --model_name_or_path /path/to/Llama-2-7b-hf \
+    --data_path /path/to/datasets/character-llm-data/prompted/shuffle.jsonl \
+    --do_train \
+    --finetuning_type lora \
+    --output_dir ./ckpt/lora/$current_time/ \
+    --max_source_length 4096 \
+    --overwrite_cache \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --learning_rate 2e-4 \
+    --num_train_epochs 1.0 \
+    --plot_loss \
+    --lora_rank 8 \
+    --fp16 \
+    --dataset character-llm
